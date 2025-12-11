cb78155ae76061122b9cc204a1c73040cc081e43
AnandaGhosh-GIT <ghoshananda4@gmail.com>
2025-12-11 15:12:50 +0530

    Hello World program

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,7 +3,6 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args)  //Main gate to execute the code
     {
-        System.out.println("Hello World");
-        //System.out.println to print something
+        System.out.println("Hello World"); //System.out.println to print something
     }
 }
