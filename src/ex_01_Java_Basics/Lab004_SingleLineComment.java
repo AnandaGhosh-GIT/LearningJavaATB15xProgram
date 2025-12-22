73701fbc1eadc04f152942b1f447c1f2f9898c58
AnandaGhosh-GIT <ghoshananda4@gmail.com>
2025-12-22 10:57:43 +0530

    Single Line Comment

--- src/ex_01_Java_Basics/Lab004_SingleLineComment.java
+++ src/ex_01_Java_Basics/Lab004_SingleLineComment.java
@@ -2,8 +2,10 @@ package ex_01_Java_Basics;
 
 public class Lab004_SingleLineComment {
     public static void main(String[] args) {
+
         //This is a comment
         // This is a single line comment
         // This will not be executed
+
     }
 }
