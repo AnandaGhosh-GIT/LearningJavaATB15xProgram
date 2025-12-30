04937190c09636a758cd66c6dadff31b89e61af4
AnandaGhosh-GIT <ghoshananda4@gmail.com>
2025-12-30 21:18:09 +0530

    Print vs Println

---
+++ src/ex_01_Java_Basics/Lab008_Print_vs_Println.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Print_vs_Println {
+    public static void main(String[] args) {
+
+        //Print-The cursor remains on the same line immediately after the printed text.
+        //      output will appear on that same line.
+        System.out.print("Hello Ananda");
+        System.out.print(" Ghosh!");
+
+        //Println- The cursor moves to the beginning of the next line after printing the text.
+        //         Subsequent output will start on a fresh line.
+        System.out.println(" Welcome to");
+        System.out.println("JAVA");
+
+    }
+}
