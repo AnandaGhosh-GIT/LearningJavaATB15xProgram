609d792f2b72fcb3b4732e743f65d7455dcca86e
AnandaGhosh-GIT <ghoshananda4@gmail.com>
2025-12-30 21:03:17 +0530

    Class Name Example

---
+++ src/ex_01_Java_Basics/Lab007_Class_Name_Example.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab007_Class_Name_Example {
+    public static void main(String[] args) {
+        System.out.println("2+2"); // Print as a String
+        System.out.println(2+2); //Print as a Sum of two numbers
+    }
+}
