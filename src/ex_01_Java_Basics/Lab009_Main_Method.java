28a879fda51e7cbe4ffd20147d3470d927d84850
AnandaGhosh-GIT <ghoshananda4@gmail.com>
2026-01-27 19:06:12 +0530

    Main Method

---
+++ src/ex_01_Java_Basics/Lab009_Main_Method.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basics;
+
+public class Lab009_Main_Method {
+    public static void main(String[] args){
+        // Only one main method will be executed
+    }
+
+   // we can declare other main methods but the signature
+    // should be different
+
+    public static void main(String args){
+
+    }
+
+    public static void Main(String[] args) {
+
+    }
+}
+
