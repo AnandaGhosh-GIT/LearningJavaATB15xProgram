b20e4d02e8f0e37f8828f7cade9d363fe94c0908
Ananda Ghosh <ghoshananda4@gmail.com>
2026-04-18 00:33:42 +0530

    Common Math Exception

--- src/ex_01_Java_Basics/Lab010_Common_Math_Exception.java
+++ src/ex_01_Java_Basics/Lab010_Common_Math_Exception.java
@@ -2,14 +2,12 @@ package ex_01_Java_Basics;
 
 public class Lab010_Common_Math_Exception {
     static void main(String[] args) {
-       // Single Qutoes are not supported in JAVA
+       // Single Quotes are not supported in JAVA
         // System.out.println('10/0');
 
         //math exception - divided by 0 will cause this error
         System.out.println(10/0);
         System.out.println(0/100);
         System.out.println(0/0);
-    }
-
     }
 }
