9de5c7a8a21df54f2a3d20871efe9ef0a761029c
Ananda Ghosh <ghoshananda4@gmail.com>
2026-04-17 22:04:24 +0530

    Keywords & Identifiers

--- src/ex_01_Java_Basics/Lab006_Keywords_Identifiers.java
+++ src/ex_01_Java_Basics/Lab006_Keywords_Identifiers.java
@@ -1,8 +1,8 @@
-package ex_01_Java_Basics;
+package ex_01_Java_Basics; //ex_01_Java_Basics- Identifiers
 
-public class Lab006_Keywords_Identifiers {
+public class Lab006_Keywords_Identifiers { //Lab006_Keywords_Identifiers- Identifiers
     public static void main(String[] args) {
         System.out.println("Keywords");
-        // package, public, class, static, void
+        //package, public, class, static, void these are Keywords
     }
 }
