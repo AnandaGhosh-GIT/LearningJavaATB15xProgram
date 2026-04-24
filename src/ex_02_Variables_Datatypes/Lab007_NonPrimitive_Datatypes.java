6c755748faf353dc209f3a6ad4512c8cdd5a421d
Ananda Ghosh <ghoshananda4@gmail.com>
2026-04-25 00:56:58 +0530

    Variable and Datatypes

---
+++ src/ex_02_Variables_Datatypes/Lab007_NonPrimitive_Datatypes.java
@@ -0,0 +1,10 @@
+package ex_02_Variables_Datatypes;
+
+public class Lab007_NonPrimitive_Datatypes {
+    public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items = new int[10]; //Array
+
+    }
+
+}
